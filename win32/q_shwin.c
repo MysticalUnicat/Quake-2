e452eb719ef8df286849daf8000e1b40cf60688f
MysticalUnicat <67205018+MysticalUnicat@users.noreply.github.com>
2022-10-10 04:36:33 -0700

    removing a bit of old code

--- win32/q_shwin.c
+++ win32/q_shwin.c
@@ -27,84 +27,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <io.h>
 #include <conio.h>
 
-//===============================================================================
-
-// int hunkcount;
-
-// byte *membase;
-// int hunkmaxsize;
-// int cursize;
-
-// #define VIRTUAL_ALLOC
-
-// void *Hunk_Begin(int maxsize) {
-//   // reserve a huge chunk of memory, but don't commit any yet
-//   cursize = 0;
-//   hunkmaxsize = maxsize;
-// #ifdef VIRTUAL_ALLOC
-//   membase = VirtualAlloc(NULL, maxsize, MEM_RESERVE, PAGE_NOACCESS);
-// #else
-//   membase = malloc(maxsize);
-//   memset(membase, 0, maxsize);
-// #endif
-//   if(!membase)
-//     Sys_Error("VirtualAlloc reserve failed");
-//   return (void *)membase;
-// }
-
-// void *Hunk_Alloc(int size) {
-//   void *buf;
-
-//   // round to cacheline
-//   size = (size + 31) & ~31;
-
-// #ifdef VIRTUAL_ALLOC
-//   // commit pages as needed
-//   //	buf = VirtualAlloc (membase+cursize, size, MEM_COMMIT, PAGE_READWRITE);
-//   buf = VirtualAlloc(membase, cursize + size, MEM_COMMIT, PAGE_READWRITE);
-//   if(!buf) {
-//     FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
-//                   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&buf, 0, NULL);
-//     Sys_Error("VirtualAlloc commit failed.\n%s", buf);
-//   }
-// #endif
-//   cursize += size;
-//   if(cursize > hunkmaxsize)
-//     Sys_Error("Hunk_Alloc overflow");
-
-//   return (void *)(membase + cursize - size);
-// }
-
-// int Hunk_End(void) {
-
-//   // free the remaining unused virtual memory
-// #if 0
-// 	void	*buf;
-
-// 	// write protect it
-// 	buf = VirtualAlloc (membase, cursize, MEM_COMMIT, PAGE_READONLY);
-// 	if (!buf)
-// 		Sys_Error ("VirtualAlloc commit failed");
-// #endif
-
-//   hunkcount++;
-//   // Com_Printf ("hunkcount: %i\n", hunkcount);
-//   return cursize;
-// }
-
-// void Hunk_Free(void *base) {
-//   if(base)
-// #ifdef VIRTUAL_ALLOC
-//     VirtualFree(base, 0, MEM_RELEASE);
-// #else
-//     free(base);
-// #endif
-
-//   hunkcount--;
-// }
-
-//===============================================================================
-
 /*
 ================
 Sys_Milliseconds
@@ -113,23 +35,13 @@ Sys_Milliseconds
 unsigned int curtime;
 unsigned int sys_milliseconds_base;
 
-void sys_milliseconds_init(void) { sys_milliseconds_base = (unsigned int)(uv_hrtime() / (uint64_t)1000000); }
+static void sys_milliseconds_init(void) { sys_milliseconds_base = (unsigned int)(uv_hrtime() / (uint64_t)1000000); }
 
 unsigned int Sys_Milliseconds(void) {
   static uv_once_t init = UV_ONCE_INIT;
   uv_once(&init, sys_milliseconds_init);
   curtime = (unsigned int)(uv_hrtime() / (uint64_t)1000000) - sys_milliseconds_base;
   return curtime;
-  // static int base;
-  // static bool initialized = false;
-
-  // if(!initialized) { // let base retain 16 bits of effectively random data
-  //   base = timeGetTime() & 0xffff0000;
-  //   initialized = true;
-  // }
-  // curtime = timeGetTime() - base;
-
-  // return curtime;
 }
 
 void Sys_Mkdir(char *path) { _mkdir(path); }
